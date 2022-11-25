4f68a7e099b12e65d2ad57b4a98e35c463ef3faf
Camoen <camoenvw@gmail.com>
2022-11-24 21:01:32 -0800

    Refactor into Minefield and Cell classes, get mouse coordinates on click

--- src/public/minefinder.js
+++ src/public/minefinder.js
@@ -3,57 +3,116 @@ document.body.appendChild(app.view);
 const Graphics = PIXI.Graphics;
 const cellSize = 15;
 
-function createCell(app, xCoord, yCoord, cellSize){
-    let cell = new Graphics();
-    cell.beginFill(0x4433AA)
-        .lineStyle(1, 0x111111, 0.35)
-        .drawRect(xCoord, yCoord, cellSize, cellSize)
-        .endFill();
-    app.stage.addChild(cell);
 
-    cell.interactive = true;
-    cell.on('mouseenter', function() {
-        cell.tint = 0x880808;
-    })
-    cell.on('mouseleave', function() {
-        cell.tint = 0xFFFFFF;
-    })
-    return cell
+class Cell {
+    constructor(app, xCoord, yCoord, cellSize){
+        this.value = "E"; // E for EMPTY
+        this.square = this.buildCell(app, xCoord, yCoord, cellSize);
+    }
+
+    buildCell(app, xCoord, yCoord, cellSize){
+        let square = new Graphics();
+        square.beginFill(0x4433AA)
+            .lineStyle(1, 0x111111, 0.35)
+            .drawRect(xCoord, yCoord, cellSize, cellSize)
+            .endFill();
+        app.stage.addChild(square);
+    
+        square.interactive = true;
+        square.on('mouseenter', function() {
+            square.tint = 0x880808;
+        })
+        square.on('mouseleave', function() {
+            square.tint = 0xFFFFFF;
+        })
+        return square;
+    }
 }
 
-function generateMinefield(app, width, height, cellSize){
-    let minefield = new Map;
-    let border = 5;
-    let boardWidth = width * cellSize;
-    let boardHeight = height * cellSize;
-    app.renderer.resize(boardWidth + border*2, boardHeight + border*2);
-    for (let x = border; x < boardWidth; x += cellSize){
-        for (let y = border; y < boardHeight; y += cellSize){
-            minefield.set(x + "|" +  y, createCell(app, x, y, cellSize));
+
+class Minefield {
+    constructor(app, width, height, cellSize, border){
+        this.app = app;
+        this.board = new Map;
+        this.border = border;
+        this.cellSize = cellSize;
+        this.boardWidth = width * cellSize;
+        this.boardHeight = height * cellSize;
+        this.createBoard(this.boardWidth, this.boardHeight, this.cellSize, this.border);
+    }
+
+    getCellKey(xCoord, yCoord){
+        let x = Math.floor((xCoord - this.border) / this.cellSize);
+        let y = Math.floor((yCoord - this.border) / this.cellSize);
+        return x + "|" +  y;
+    }
+
+    /**
+     * Initializes the game board (this.board), which maps coordinates to Cell objects:
+     * this.board = {'0|0': Cell(), '15|15': Cell(), ...} 
+     * 
+     * @param {Number} boardWidth   Number of cells along x-axis of game board
+     * @param {Number} boardHeight  Number of cells along y-axis of game board
+     * @param {Number} cellSize     Height and Width of one cell
+     * @param {Number} border       Offset to provide a border around the game board
+     */
+    createBoard(boardWidth, boardHeight, cellSize, border) {
+        this.app.renderer.resize(boardWidth + border*2, boardHeight + border*2);
+        for (let x = border; x < boardWidth; x += cellSize){
+            for (let y = border; y < boardHeight; y += cellSize){
+                this.board.set(this.getCellKey(x, y), new Cell(this.app, x, y, cellSize));
+            }
         }
     }
-    return minefield;
+
+    revealCells(xCoord, yCoord){
+        let minX = this.border; 
+        let minY = this.border;
+        let maxX = this.boardWidth * this.cellSize + this.border;
+        let maxY = this.boardHeight * this.cellSize + this.border;
+        let step = this.cellSize;
+        // We may reveal cells in all 8 directions
+        let directions = [[-1, -1], [-1, 0], [-1, 1], 
+                          [ 0, -1], [ 0, 1],
+                          [ 1, -1], [ 1, 0], [ 1, 1]];
+
+        console.log(this.getCellKey(xCoord, yCoord));
+        console.log(this.board.get(this.getCellKey(xCoord, yCoord)).square.vertexData);
+        console.log(this.board.get(this.getCellKey(xCoord, yCoord)).value);
+        console.log("TODO: Implement DFS :)");
+
+    }
 }
 
 let minefield;
 // TODO: Allow user to select mode.
 let mode = "expert";
 if (mode == "beginner"){
-    minefield = generateMinefield(app, 9, 9, cellSize);
+    minefield = new Minefield(app, 9, 9, cellSize, 5);
+    // minefield = generateMinefield(app, 9, 9, cellSize);
 } else if (mode == "intermediate") {
-    minefield = generateMinefield(app, 16, 16, cellSize);
+    minefield = new Minefield(app, 16, 16, cellSize, 5);
+    // minefield = generateMinefield(app, 16, 16, cellSize);
 } else if (mode == "expert"){
-    minefield = generateMinefield(app, 30, 16, cellSize);
+    minefield = new Minefield(app, 30, 16, cellSize, 5);
+    // minefield = generateMinefield(app, 30, 16, cellSize);
 }
 
 // Example of how to override tint for a particular cell
-minefield.get("5|5").on('mouseenter', function() {
+minefield.board.get(minefield.getCellKey(5, 5)).square.on('mouseenter', function() {
     this.tint = 0x00FF00;
 });
-minefield.get("5|20").tint = 0xAAAAAA;
-minefield.get("20|20").tint = 0xAAAAAA;
 
+// On click, reveal cells on game board
+app.stage.on('pointerup', function(mouseData) {
+    console.log('X', mouseData.data.global.x, 'Y', mouseData.data.global.y);
+    minefield.revealCells(mouseData.data.global.x, mouseData.data.global.y);
+});
 
+// app.stage.addEventListener('click', function handleClick()
+// {
+//     console.log('Hello world!');
+// });
 
 /*
 // Begin Tutorial Code
