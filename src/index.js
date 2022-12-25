7b8408fce79aee4f51c9ceb4f2bfb1c21145d204
Camoen <camoenvw@gmail.com>
2022-12-24 23:53:36 -0800

    Add create room, join room, and leave room functionalities

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
 });
 
 var usernames = {};
+var rooms = {};
+
 socketio.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
@@ -19,15 +21,43 @@ socketio.on('connection', (socket) => {
     delete usernames[socket.username];
   });
 
+  socket.on('room-created', (roomName, username)=>{
+		rooms[roomName] = new Set();
+    rooms[roomName].add(username);
+    socketio.emit('update-room-list',  Object.keys(rooms));
+		console.log("Rooms: ", rooms);
+	});
+
   socket.on('user-created', (username)=>{
 		socket.username = username;
 		usernames[username] = username;
+    socketio.to(socket.id).emit('update-room-list',  Object.keys(rooms));
+    console.log("rooms: ", rooms);
 		console.log("Users: ", usernames);
 	});
 
   socket.on('user-deleted', (username)=>{
 		delete usernames[socket.username];
 		console.log("Users: ", usernames);
+	});
+
+  socket.on('user-joined-room', (roomName, username)=>{
+		rooms[roomName].add(username);
+		console.log("Rooms: ", rooms);
+	});
+
+  socket.on('user-left-room', (roomName, username)=>{
+		rooms[roomName].delete(username);
+    if (rooms[roomName].size === 0){
+      // If a room is empty, delete it and push the room's termination to all active users
+      delete rooms[roomName];
+      socketio.emit('update-room-list',  Object.keys(rooms));
+    } else {
+      // Othwerise, only update available rooms for the user that has left the room
+      socketio.to(socket.id).emit('update-room-list',  Object.keys(rooms));
+    }
+    
+		console.log("Rooms: ", rooms);
 	});
 });
 
